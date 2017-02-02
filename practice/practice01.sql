5b878c6e61f4a5e397222f68a6a0b956f4faa4d2
bokyung1500 <bokyung1500@naver.com>
2017-02-02 14:48:07 +0900

    sql 연습문제1

--- practice/practice01.sql
+++ practice/practice01.sql
@@ -5,21 +5,20 @@ select first_name || ' ' || last_name as "이름", email as "이메일",
 phone_number as "전화번호" from employees order by hire_date asc;
 
 --2
-select j.job_title as "업무 이름", max(e.salary) as "최고임금" 
-from employees e, jobs j where e.job_id = j.job_id group by job_title order by 최고임금 desc;
+select job_title as "업무 이름", max_salary as "최고임금" 
+from jobs order by max_salary desc;
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
 --3
 select count(*) from employees where manager_id is null;
 
 --4
-select job_id as "JOB_TITLE" from employees group by job_id 
-order by max(salary) desc;
+select job_title from jobs order by max_salary desc;
 
 --5
-select distinct count(job_id) from employees;
+select distinct count(department_id) from departments;
 
 --6
-select job_id from employees group by job_id order by length(job_id) desc; 
+select department_name from departments order by length(department_name) desc; 
 
 --7
 select count(*) as "매니저없는부서" from employees where manager_id is null;
@@ -35,3 +34,5 @@ select region_name from regions order by length(region_name) asc;
 select lower(l.city) from departments d, locations l 
 where d.LOCATION_ID = l.LOCATION_ID
 group by l.city order by l.city asc;
+
+select lower(city) from locations order by city asc;
